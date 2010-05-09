4b311fa6d90eebd17f2f124e0ae8e6b6077dc1e0
Ricardo Alves <Administrator@a.7@gmail.com>
2010-05-09 20:39:59 +0100

    Modificaçoes no WebService (Muito Incompleto)
    Adiçao do primeiro Wraper (de muitos!!! '-_-)

---
+++ src/Newzic/Newzic.WebService/Newzic.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+using Newzic.Data;
+using System.Collections;
+using Pericles.WebService;
+
+namespace Newzic.WebService
+{
+    /// <summary>
+    /// Summary description for Service
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Newzic : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public List<NoticiaWrap> searchNoticia(string query, string type) {
+            List<NoticiaWrap> ret = null;
+            //Noticia n = new Noticia();
+            //n.no
+            return ret;
+        }
+    }
+}
---
+++ src/Newzic/Newzic.WebService/NoticiaWrap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newzic.Data;
+
+//{ }
+namespace Pericles.WebService {
+
+    public class NoticiaWrap {
+
+        private Guid NoticiaId { get; set; }
+        private string Titulo { get; set; }
+        private string Corpo { get; set; }
+        private int Pontuacao { get; set; }
+        private DateTime Data { get; set; }
+        private int FlagCount { get; set; }
+        private bool Deleted { get; set; }
+        private bool Marked { get; set; }
+        private Guid JornalistaId { get; set; }
+
+        public NoticiaWrap() {
+            this.NoticiaId = Guid.Empty;
+            this.Titulo = null;
+            this.Corpo = null;
+            this.Pontuacao = 0;
+            this.Data = DateTime.MinValue;
+            this.FlagCount = 0;
+            this.Deleted = false;
+            this.Marked = false;
+            this.JornalistaId = Guid.Empty;
+        }
+
+        public NoticiaWrap(Noticia n) {
+            this.NoticiaId = n.NoticiaId;
+            this.Titulo = n.Titulo;
+            this.Corpo = n.Corpo;
+            this.Pontuacao = n.Pontuacao;
+            this.Data = n.Data;
+            this.FlagCount = n.FlagCount;
+            this.Deleted = n.Deleted;
+            this.Marked = n.Marked;
+            this.JornalistaId = n.JornalistaId;
+        }
+
+    }
+}
