2d749cecae400b88ed36ba95f3ed642745c3e695
Christophe <Christophe@.(none)>
2010-05-09 20:40:52 +0100

    Implementação de algumas classes e metodos(apenas com NotImplementedException) da camada de dados.

---
+++ src/Newzic/Newzic.Data/NoticiaData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Newzic.Data
+{
+    public class NoticiaData
+    {
+        public Noticia fetchNoticia(Guid noticiaID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Noticia> fetchNoticias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void createNoticia(Noticia noticia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateNoticia(Noticia noticia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void removeNoticia(Guid noticiaID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void markNoticia(Guid noticiaID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void flagNoticia(Guid noticiaID)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
