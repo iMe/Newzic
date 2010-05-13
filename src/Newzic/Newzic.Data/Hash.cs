315d69d6aca63f5ce2b1a6799fb4d62a099a6b36
Ricardo Alves <Administrator@a.7@gmail.com>
2010-05-13 18:36:02 +0100

    Class Hash.cs adicionada

---
+++ src/Newzic/Newzic.Data/Hash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Newzic.Core
+{
+    public static class Hash
+    {
+        public static String generate(String pass)
+        {
+            SHA1 sha = SHA1.Create();
+            byte[] result = sha.ComputeHash(Encoding.UTF8.GetBytes(pass));
+            return Convert.ToBase64String(result);
+        }
+    }
+}
