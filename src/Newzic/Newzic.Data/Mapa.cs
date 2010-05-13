b8d4f0291abeac65fe88c48464e8dc645e0eaa3f
Christophe <Christophe@.(none)>
2010-05-13 17:50:52 +0100

    Correção do DBML Criação da classe Mapa.cs

---
+++ src/Newzic/Newzic.Data/Mapa.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Newzic.Core
+{
+    public partial class Mapa : IEntity
+    {
+        public void InsertPonto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertTour()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid Id
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
