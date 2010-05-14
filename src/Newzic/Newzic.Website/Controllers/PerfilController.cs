588396c87fc1c3d342fa98c4bc4478992896d5b4
Ricardo Alves <Administrator@a.7@gmail.com>
2010-05-14 19:20:57 +0100

    Prototipos dos Controllers criados

---
+++ src/Newzic/Newzic.Website/Controllers/PerfilController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Newzic.Website.Controllers
+{
+    public class PerfilController : Controller
+    {
+        //
+        // GET: /Perfil/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
