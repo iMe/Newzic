b16caae670139ddf2776bf59b3cff8238567473c
Christophe <Christophe@.(none)>
2010-05-22 18:35:58 +0100

    Implementei isbanned e corrigi outros metodos Adicionei Views de Index e GerirJornalistas.aspx para o Moderador.cs

--- src/Newzic/Newzic.Data/Moderador.cs
+++ src/Newzic/Newzic.Data/Moderador.cs
@@ -37,5 +37,19 @@ namespace Newzic.Core
             banido.DataFim = dataFim;
             jornalista.Banidos.Add(banido);
         }
+
+        public bool isBanned()
+        {
+            var jornalista = repJornalistas.fetchAll().Single(n => n.JornalistaId == this.ModeradorId);
+            try
+            {
+                var a = jornalista.Banidos.Single(n => n.JornalistaId == this.JornalistaId);
+                return true;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        }
     }
 }
