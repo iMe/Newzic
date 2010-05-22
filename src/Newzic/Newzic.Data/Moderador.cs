20acf4220eba591762d4fd2de9342722f5a3153e
Christophe <Christophe@.(none)>
2010-05-22 17:13:02 +0100

    Copia da view AcessoNegado.aspx colocada na Pasta Shared das Views Correções devidas a remoção da herança de Administrador.cs e Moderador.cs

--- src/Newzic/Newzic.Data/Moderador.cs
+++ src/Newzic/Newzic.Data/Moderador.cs
@@ -1,13 +1,41 @@
 using System;
+using System.Linq;
 
 namespace Newzic.Core
 {
     public partial class Moderador : IEntity
     {
+        private readonly IDataCRUD<Jornalista> repJornalistas = new DataCRUD<Jornalista>();
+
         Guid IEntity.Id
         {
             get { return ModeradorId; }
             set { ModeradorId = value; }
+        }
+
+        public void Ban()
+        {
+            var banido = new Banido();
+            var jornalista = repJornalistas.fetchAll().Single(n => n.JornalistaId == this.ModeradorId);
+
+            banido.BanidoId = Guid.NewGuid();
+            banido.Permanente = true;
+            banido.Jornalista = jornalista;
+            banido.JornalistaId = jornalista.JornalistaId;
+            jornalista.Banidos.Add(banido);
+        }
+
+        public void Ban(DateTime dataFim)
+        {
+            var banido = new Banido();
+            var jornalista = repJornalistas.fetchAll().Single(n => n.JornalistaId == this.ModeradorId);
+
+            banido.BanidoId = Guid.NewGuid();
+            banido.Permanente = false;
+            banido.Jornalista = jornalista;
+            banido.JornalistaId = jornalista.JornalistaId;
+            banido.DataFim = dataFim;
+            jornalista.Banidos.Add(banido);
         }
     }
 }
