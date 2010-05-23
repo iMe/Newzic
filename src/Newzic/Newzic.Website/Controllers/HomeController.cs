e3b05daa7e8dd2df3baf266de7efbddd2c28c872
Ricardo Alves <Administrator@a.7@gmail.com>
2010-05-23 23:56:02 +0100

    Comecei a trabalhar na home page

--- src/Newzic/Newzic.Website/Controllers/HomeController.cs
+++ src/Newzic/Newzic.Website/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newzic.Core;
+using Newzic.Website.Models;
 
 namespace Newzic.Website.Controllers
 {
@@ -13,13 +15,33 @@ namespace Newzic.Website.Controllers
         {
 
             ViewData["Message"] = "Welcome to ASP.NET MVC!";
+            HomeModel model = new HomeModel();
+            model.noticias = getNoticias();
 
-            return View();
+            return View("Index",model);
         }
 
         public ActionResult About()
         {
             return View();
+        }
+
+
+        private List<Noticia> getNoticias()
+        {
+            IDataCRUD<Noticia> data = new DataCRUD<Noticia>();
+            var ns = data.fetchAll();
+            List<Noticia> res = new List<Noticia>();
+            
+            foreach (Noticia noticia in ns)
+            {
+                noticia.calcRank();
+                res.Add(noticia);
+            }
+
+            res.OrderBy(x => x.rank).Reverse();
+            //ns = from Noticia n in ns orderby n.rank select n;
+            return res;
         }
 
     }
