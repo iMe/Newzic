5b0e3f517ca5753d8593baa2a1f7b5fe5effb288
Vitor <vitor@.(none)>
2010-05-27 22:31:50 +0100

    commit do SiteModel.cs

---
+++ src/Newzic/Newzic.Website/Models/SiteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Newzic.Website.Models
+{
+    public class SiteModel : Controller
+    {
+        public string viewerRole { get; set; }
+    }
+}
